a21bf0fd3649c88947c111abf800ab4c415c2ccb
Justin10612 <atom.9031@gmail.com>
2023-10-12 23:23:50 +0800

    Button Test

--- rb_controller/robot_controller.py
+++ rb_controller/robot_controller.py
@@ -2,21 +2,40 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from sensor_msgs.msg import Joy
 
 
 class RobotController(Node):
+    # Parameter
+    __FOLLOW_MODE_BTN = 1
+    __TELEOP_MODE_BTN = 2
 
     def __init__(self):
         super().__init__('robot_controller')
+        # Create publisher
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.subscription = self.create_subscription(String, 'topic', self.listener_callback, 10)
+        # Create subscriber
+        self.subscription = self.create_subscription(Joy, 'joy', self.button_read, 10)
         self.subscription  # prevent unused variable warning
 
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
+    def button_read(self, joy_msg):
+        follow_mode_flag = True
+        teleop_mode_flag = True
+        # Follow mode btn
+        if joy_msg.buttons[self.__FOLLOW_MODE_BTN] == 1 and follow_mode_flag :
+            follow_mode_flag = False
+            self.get_logger().info('Follow_mode')
+        else :
+            follow_mode_flag = True
+        # Teleop operate mode btn
+        if joy_msg.buttons[self.__TELEOP_MODE_BTN] == 1 and teleop_mode_flag :
+            teleop_mode_flag = False
+            self.get_logger().info('Teleop_mode')
+        else:
+            teleop_mode_flag = True
 
     def timer_callback(self):
         msga = String()
@@ -25,14 +44,14 @@ class RobotController(Node):
         self.get_logger().info('Publishing: "%s"' % msga.data)
         self.i += 1
 
+    # def mode_FSM():
+        
+
 
 def main(args=None):
     rclpy.init(args=args)
-
     robot_controller = RobotController()
-
     rclpy.spin(robot_controller)
-
     # Destroy the node explicitly
     # (optional - otherwise it will be done automatically
     # when the garbage collector destroys the node object)
