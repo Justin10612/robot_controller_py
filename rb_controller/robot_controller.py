bca2676a769f7a670c0430a6b136b61fb5934567
Justin10612 <atom.9031@gmail.com>
2023-10-13 05:07:29 +0800

    what a mess

--- rb_controller/robot_controller.py
+++ rb_controller/robot_controller.py
@@ -2,51 +2,71 @@ import rclpy
 from rclpy.node import Node
 
 from std_msgs.msg import String
+from std_msgs.msg import Bool
 from sensor_msgs.msg import Joy
 
 
 class RobotController(Node):
     # Parameter
-    __FOLLOW_MODE_BTN = 1
-    __TELEOP_MODE_BTN = 2
-    follow_mode_flag = True
-    teleop_mode_flag = True
+    __FOLLOW_MODE_BTN = 1 #a
+    __TELEOP_MODE_BTN = 2 #b
+    __IDLE_MODE_BTN = 3 #y
+    # Mode
+    follow_mode_state = False
+    teleop_mode_state = False
+    idle_mode_state = True
+    target_state = True
+    # Button State
+    follow_btn_flag = True
+    teleop_btn_flag = True
+    idle_btn_flag = True
 
     def __init__(self):
         super().__init__('robot_controller')
         # Create publisher
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        self.idle_bool_pub = self.create_publisher(Bool, 'idle_bool', 10)
+        self.follow_bool_pub = self.create_publisher(Bool, 'follow_bool', 10)
+        self.teleop_bool_pub = self.create_publisher(Bool, 'teleop_bool', 10)
         # Create subscriber
         self.subscription = self.create_subscription(Joy, 'joy', self.button_read, 10)
         self.subscription  # prevent unused variable warning
 
     def button_read(self, joy_msg):
-        # Follow mode btn
-        if joy_msg.buttons[self.__FOLLOW_MODE_BTN] == 1 and self.follow_mode_flag :
-            self.follow_mode_flag = False
-            self.get_logger().info('Follow_mode')
-        elif joy_msg.buttons[self.__FOLLOW_MODE_BTN] == 0 and not(self.follow_mode_flag):
-            self.follow_mode_flag = True
-        # Teleop operate mode btn
-        if joy_msg.buttons[self.__TELEOP_MODE_BTN] == 1 and self.teleop_mode_flag :
-            self.teleop_mode_flag = False
-            self.get_logger().info('Teleop_mode')
-        elif joy_msg.buttons[self.__TELEOP_MODE_BTN] == 0 and not(self.teleop_mode_flag):
-            self.teleop_mode_flag = True
+        # ############# Idle operate mode btn #############
+        if joy_msg.buttons[self.__IDLE_MODE_BTN] == 1 and self.idle_btn_flag :
+            self.idle_btn_flag = False
+            self.get_logger().info('IDLE_btn_pressed')
+            # Clear status
+            self.idle_mode_state = True
+            self.follow_mode_state = False
+            self.teleop_mode_state = False
+        elif joy_msg.buttons[self.__IDLE_MODE_BTN] == 0 and not(self.idle_btn_flag):
+            self.idle_btn_flag = True
+        # ############# Follow btn btn ####################
+        if joy_msg.buttons[self.__FOLLOW_MODE_BTN] == 1 and self.follow_btn_flag :
+            self.follow_btn_flag = False
+            self.get_logger().info('Follow_btn_pressed')
+            # Change State
+            self.idle_mode_state = False
+            self.follow_mode_state = True
+        elif joy_msg.buttons[self.__FOLLOW_MODE_BTN] == 0 and not(self.follow_btn_flag):
+            self.follow_btn_flag = True
+        # ############# Teleop operate btn btn #############
+        if joy_msg.buttons[self.__TELEOP_MODE_BTN] == 1 and self.teleop_btn_flag :
+            self.teleop_btn_flag = False
+            self.get_logger().info('Teleop_btn_pressed')
+            # Change State
+            self.idle_mode_state = False
+            self.teleop_mode_state = True
+        elif joy_msg.buttons[self.__TELEOP_MODE_BTN] == 0 and not(self.teleop_btn_flag):
+            self.teleop_btn_flag = True
 
-    def timer_callback(self):
-        msga = String()
-        msga.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msga)
-        self.get_logger().info('Publishing: "%s"' % msga.data)
-        self.i += 1
-
-    # def mode_FSM():
-        
-
+        if self.idle_mode_state==True :
+            self.get_logger().info('IDLE')
+        elif self.follow_mode_state==True and self.target_state==True and self.teleop_mode_state==False:
+            self.get_logger().info('FOLLOW')
+        elif self.teleop_mode_state==True and self.follow_mode_state==False:
+            self.get_logger().info('TELEOP')
 
 def main(args=None):
     rclpy.init(args=args)
